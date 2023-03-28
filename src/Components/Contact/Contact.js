727c0287fa3d416623a950058caa10c684ca9fd7
belvl <belvl@tomskcable.ru>
2023-03-28 18:06:08 +0700

    Добавлена интерактивная гугл-карта

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -21,20 +21,20 @@ function Contact() {
                     </div>
                 </div>
             </div>
-            {/*<div className={styles.map} style={{position:"relative", overflow: "hidden"}}><a*/}
-            {/*    href="https://yandex.ru/maps/org/ooo_tomskiy_kabelny_zavod/1167283128/?utm_medium=mapframe&utm_source=maps"*/}
-            {/*    style={{color: "#eee", fontSize: "12px", position: "absolute", top:"0px"}}>ООО Томский кабельный завод</a><a*/}
-            {/*    href="https://yandex.ru/maps/67/tomsk/category/cables_and_wires/184107032/?utm_medium=mapframe&utm_source=maps"*/}
-            {/*    style={{color: "#eee", fontSize:"12px", position: "absolute", top: "14px"}}>Кабель и провод в Томске</a><a*/}
-            {/*    href="https://yandex.ru/maps/67/tomsk/category/cement/184107765/?utm_medium=mapframe&utm_source=maps"*/}
-            {/*    style={{color: "#eee", fontSize: "12px", position: "absolute", top: "28px"}}>Цемент в Томске</a>*/}
-            {/*    <iframe*/}
-            {/*        src="https://yandex.ru/map-widget/v1/?ll=84.990790%2C56.524788&mode=poi&poi%5Bpoint%5D=84.990433%2C56.525304&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1167283128&z=16.61"*/}
-            {/*        width="760" height="288" frameBorder="1" allowFullScreen="true" style={{position: "relative"}}></iframe>*/}
-            {/*</div>*/}
-            <div className={styles.map}>
-                <img width={760} height={288} src="/img/Contact/map.svg" alt="Карта"/>
+            <div className={styles.map} style={{position:"relative", overflow: "hidden"}}><a
+                href="https://yandex.ru/maps/org/ooo_tomskiy_kabelny_zavod/1167283128/?utm_medium=mapframe&utm_source=maps"
+                style={{color: "#eee", fontSize: "12px", position: "absolute", top:"0px"}}>ООО Томский кабельный завод</a><a
+                href="https://yandex.ru/maps/67/tomsk/category/cables_and_wires/184107032/?utm_medium=mapframe&utm_source=maps"
+                style={{color: "#eee", fontSize:"12px", position: "absolute", top: "14px"}}>Кабель и провод в Томске</a><a
+                href="https://yandex.ru/maps/67/tomsk/category/cement/184107765/?utm_medium=mapframe&utm_source=maps"
+                style={{color: "#eee", fontSize: "12px", position: "absolute", top: "28px"}}>Цемент в Томске</a>
+                <iframe
+                    src="https://yandex.ru/map-widget/v1/?ll=84.990790%2C56.524788&mode=poi&poi%5Bpoint%5D=84.990433%2C56.525304&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1167283128&z=16.61"
+                    width="760" height="288" frameBorder="1" allowFullScreen="true" style={{position: "relative", border: "none"}}></iframe>
             </div>
+            {/*<div className={styles.map}>*/}
+            {/*    <img width={760} height={288} src="/img/Contact/map.svg" alt="Карта"/>*/}
+            {/*</div>*/}
         </div>
     );
 }
